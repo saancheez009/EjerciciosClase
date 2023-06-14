bfb1a8ddcc482ade436df3c06cbe820650c31333
Britany <Britany@DESKTOP-S9E8VDF.home>
2023-06-14 23:29:24 +0200

    act

--- EjerciciosClase/src/ejercicio2/Electrodoméstico.java
+++ EjerciciosClase/src/ejercicio2/Electrodoméstico.java
@@ -1,5 +1,50 @@
 package ejercicio2;
 
-public class Electrodoméstico {
+public class Electrodoméstico1 {
+	
+protected int codigo;
+protected int precioBase;
+protected String color;
+protected String consumoEnergetico;
+protected int peso;
 
+
+
+
+
+
+
+
+public Electrodoméstico1(int codigo, int precioBase, String color, String consumoEnergetico, int peso) {
+	super();
+	this.codigo = codigo;
+	this.precioBase = precioBase;
+	this.color = color;
+	this.consumoEnergetico = consumoEnergetico;
+	this.peso = peso;
+}
+
+
+
+
+public void Electrodoméstico() {
+	
+	this.precioBase = 100;
+	this.color = "blanco";
+	this.consumoEnergetico = "F";
+	this.peso = 5;
+}
+
+
+
+
+public static String comprobarColor(String color){
+    switch (color.toLowerCase()){
+        case "blanco", "negro","rojo", "azul", "gris":
+            break;
+        default:
+            color = "blanco";
+    }
+    return color;
+}
 }
